1a2faa90e51aa6a015cfc69ddbc9841785da6e36
nanobot2000 <marginal3452@yandex.ru>
2018-06-26 15:22:40 +0300

    added requirements and main funtionality

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,70 @@
+import whois
+import argparse
+import requests
+from datetime import datetime
+from urllib.parse import urlparse
+
+
+def get_command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        '--filepath',
+        help='full path to the file with urls',
+        required=True
+    )
+    args = parser.parse_args()
+    return args
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as file:
+        urls = file.readlines()
+    return [url.strip() for url in urls]
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return requests.get(url, timeout=(10, 10)).status_code == requests.codes.ok
+    except requests.exceptions.RequestException:
+            return False
+
+
+def is_expiry_date_close(expiration_date, min_expiration_period):
+    time_left = expiration_date - datetime.now()
+    return time_left.days < min_expiration_period
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    expiration_date = domain_info.expiration_date
+    if expiration_date is None:
+        return None
+    elif isinstance(expiration_date, list):
+        return expiration_date[0]
+    else:
+        return expiration_date
+
+
+def print_site_health(url, server_response, expiration_date):
+    print(f'Checking {url}:')
+    print(f'Server respond with 200: {server_response}')
+    if expiration_date is None:
+        print(f'No expiry date for {url}')
+    else:
+        print('Expiring in a month: {}\n'.format(
+            is_expiry_date_close(
+                expiration_date,
+                min_expiration_period=31
+            )
+        ))
+
 
 if __name__ == '__main__':
-    pass
+    args = get_command_line_args()
+    loaded_urls = load_urls4check(args.filepath)
+    for url in loaded_urls:
+        domain_name = urlparse(url).netloc
+        domain_expiration_date = get_domain_expiration_date(domain_name)
+        server_response = is_server_respond_with_200(url)
+        print_site_health(url, server_response, domain_expiration_date)
